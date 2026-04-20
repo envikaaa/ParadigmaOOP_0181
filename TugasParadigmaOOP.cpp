bf9688737cfeb79dd22bdd5ad910b4a4aa6fad1e
envikaaa <envikapriyatna31@gmail.com>
2026-04-20 14:42:44 +0700

    menambahkan cout

--- TugasParadigmaOOP.cpp
+++ TugasParadigmaOOP.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 class Barang{
 public :
-    string nama;
-    int jumlah;
-    string kategori;
-    string tanggalProduksi;
+    string Nama;
+    int Jumlah;
+    string Kategori;
+    string TanggalProduksi;
 
-    void printData()
+    void printData(){
+        cout << "Nama : " << nama << endl;
+        cout << "Jumlah : " << jumlah << endl;
+        cout << "Kategori : " << kategori << endl;
+        cout << "TanggalProduksi : " << tanggalProduksi << endl;
+    }
 }
